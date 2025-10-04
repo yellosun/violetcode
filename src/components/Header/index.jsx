24753d19fc9ae2fa69dfbcc6782919c109d436e6
yellosun <violet.wmoon@gmail.com>
2025-10-04 12:20:18 -0500

    update header with email copy and paster

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,30 @@
 import clsx from "clsx";
+import { useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import { routesFlat } from "../../routes";
-import { CopyIcon, PersonIcon } from "../../assets/icons";
+import { MailIcon, CopyIcon, PersonIcon } from "../../assets/icons";
 
 export default function Header() {
+  const [codeCopied, setCodeCopied] = useState(false);
+
+  const copyCode = () => {
+    if (!codeCopied) {
+      setCodeCopied(true);
+
+      // Copy the text inside the text field
+      navigator.clipboard.writeText('violetwmoon@gmail.com');
+    }
+  };
+
+  useEffect(() => {
+    if (codeCopied) {
+      setTimeout(() => {
+        setCodeCopied(false);
+      }, 1500);
+    }
+  }, [codeCopied]);
   const Seperator = () => <span className="px-4 hidden md:flex">{"/"}</span>;
+
   return (
     <code className={container}>
       <HashLink smooth to={"#" + routesFlat.landing} className={baseLinkStyle}>
@@ -12,18 +32,29 @@ export default function Header() {
       </HashLink>
       <div className="flex items-center md:mr-10">
         <div className="flex items-center mr-4">
+          <div
+            onClick={copyCode}
+            className={clsx(
+              (codeCopied ? "text-green border-green " : "") + iconStyle
+            )}
+          >
+            <MailIcon className="mr-2 h-4 w-4" />
+
+            {codeCopied ? <span>Copied!</span> : <span>Email</span>}
+          </div>
+          <Seperator />
           <a
             target="_blank"
-            className={iconStyle}
+            className={desktopOnlyIcon}
             href="https://www.linkedin.com/in/violetmoon/"
           >
             <PersonIcon className="mr-2 h-4 w-4" />
             LinkedIn
           </a>
-          <span className="px-4">{"/"}</span>
+          <Seperator />
           <a
             target="_blank"
-            className={iconStyle}
+            className={desktopOnlyIcon}
             href="https://www.github.com/yellosun"
           >
             <CopyIcon className="mr-2 h-4 w-4" /> GitHub
@@ -41,5 +72,6 @@ const baseLinkStyle = clsx(
   "flex w-[200px] bg-gradient-to-r from-red to-orange justify-center items-center"
 );
 const iconStyle = clsx(
-  "flex italic px-4 py-2 items-center hover:underline text-xs hover:shadow-md px-6 py-2"
+  "flex italic px-4 py-2 items-center hover:underline text-xs hover:shadow-md px-6 py-2 cursor-pointer"
 );
+const desktopOnlyIcon = clsx(iconStyle, "hidden md:flex");
